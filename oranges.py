eb05a47986e8a41d98dd5ee6e8e35275fc3d4b15
Arsenii <arsa1200@yandex.ru>
2023-09-15 17:26:53 +0300

    first

--- oranges.py
+++ oranges.py
@@ -1,2 +1,12 @@
-mem = "Arsenii"
-print(mem)
+import requests
+
+headers = {"Content-Type":"application/json", "token": "64fb4ccc14fd064fb4ccc14fd3"}
+get
+response = requests.get("https://datsorange.devteam.games/buyStock", headers=headers)
+
+print(response.json())
+
+# Press the green button in the gutter to run the script.
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
