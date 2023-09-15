56271f23eb67565b61675e1c9498d27f7b702ba1
Arsenii <arsa1200@yandex.ru>
2023-09-15 17:39:34 +0300

    second

--- oranges.py
+++ oranges.py
@@ -1,8 +1,18 @@
 import requests
 
 headers = {"Content-Type":"application/json", "token": "64fb4ccc14fd064fb4ccc14fd3"}
-get
-response = requests.get("https://datsorange.devteam.games/buyStock", headers=headers)
+getScore = "https://datsorange.devteam.games/info"
+getLatestNews = "https://datsorange.devteam.games/news/LatestNews"
+getLatest5MinNews = "https://datsorange.devteam.games/news/LatestNews5Minutes"
+getLatest1MinNews = "https://datsorange.devteam.games/news/LatestNews1Minute"
+getStockSell = "https://datsorange.devteam.games/sellStock"
+getStockBuy = "https://datsorange.devteam.games/buyStock"
+getStockAll = "https://datsorange.devteam.games/getSymbols"
+bestBuy = "https://datsorange.devteam.games/BestPriceBuy"
+
+buyBestPrice = {"symbolId": 2, "quantity": 10}
+
+response = requests.post(bestBuy, headers=headers, json=buyBestPrice)
 
 print(response.json())
 
